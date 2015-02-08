367bfce08265c2f25ec9c104cd98cd8b983f616e
Cory Boyd <corytheboyd@gmail.com>
2015-02-08 14:11:28 -0800

    Undo/redo commands. Show when in command mode

--- zsh/custom/vi_mode.zsh
+++ zsh/custom/vi_mode.zsh
@@ -9,4 +9,21 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
+bindkey -a u undo
+bindkey -a '^R' redo
 export KEYTIMEOUT=1
+
+precmd() {
+  RPROMPT=""
+}
+zle-keymap-select() {
+  RPROMPT=""
+  [[ $KEYMAP = vicmd ]] && RPROMPT="[ CMD ]"
+  () { return $__prompt_status }
+  zle reset-prompt
+}
+zle-line-init() {
+  typeset -g __prompt_status="$?"
+}
+zle -N zle-keymap-select
+zle -N zle-line-init
