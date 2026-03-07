a5e4aee3a584d579ce6445ebcdd68970848d9df2
Cory Boyd <me@corytheboyd.com>
2026-03-07 11:24:18 -0700

    Update .config/VSCodium/User/settings.json Update .config/chezmoi-corytheboyd/archlinux/pkglist.txt Update .config/chezmoi-corytheboyd/shell/functions/001_os_is.zsh Add .config/chezmoi-corytheboyd/shell/lib/logger.zsh Update 001_bootstrap_archlinux.sh Update 001_bootstrap_darwin.sh Add test/archlinux/Dockerfile Update test/bin/archlinux.sh Update test/docker-compose.yml Change attributes of .config/chezmoi-corytheboyd/shell/lib/install.zsh

--- test/bin/archlinux.sh
+++ test/bin/archlinux.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
-docker compose run -it --rm --build --remove-orphans archlinux "$@"
+docker compose run -it --rm --build --remove-orphans archlinux \
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --source /chezmoi-source --apply
