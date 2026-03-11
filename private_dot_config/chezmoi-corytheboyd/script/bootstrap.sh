06d096e36ff2fefaf869bdac8890150364655eb7
Cory Boyd <me@corytheboyd.com>
2026-03-11 13:09:56 -0600

    Update .chezmoiignore Remove .config/chezmoi-corytheboyd/script/bootstrap.sh

--- private_dot_config/chezmoi-corytheboyd/script/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-
-function install_oh_my_zsh() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-}
-
-install_oh_my_zsh
